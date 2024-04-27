788860b9dcc783c1f062a4d493ec00b81b00cf35
lukwalczak <73855867+lukwalczak@users.noreply.github.com>
2024-04-27 13:21:03 +0200

    initial classes

--- Car.cs
+++ Car.cs
@@ -2,5 +2,20 @@
 
 public class Car
 {
-    
+    private string model;
+
+    private int year;
+
+    private Engine engine;
+
+    public Car(string model, int year)
+    {
+        this.model = model;
+        this.year = year;
+    }
+
+    public Car(string model, int year, Engine engine) : this(model, year)
+    {
+        this.engine = engine;
+    }
 }
--- Engine.cs
+++ Engine.cs
@@ -2,5 +2,14 @@
 
 public class Engine
 {
-    
+    private double displacement { get; set; }
+    private double horsePower { get; set; }
+    private string model { get; set; }
+
+    public Engine(double displacement = default, double horsePower = default, string model = null)
+    {
+        this.displacement = displacement;
+        this.horsePower = horsePower;
+        this.model = model;
+    }
 }
